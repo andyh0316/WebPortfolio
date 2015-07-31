89dafbd902036f8c86362fb2655d57ca7f2eff43
andyh0316 <andyhong0316@yahoo.com>
2015-07-31 15:18:50 -0700

    First Commit

---
+++ WebPortfolio/Controllers/MainController.cs
@@ -0,0 +1,40 @@
+﻿using JBabyUSA_Final.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebPortfolio.Controllers
+{
+    public class MainController : Controller
+    {
+        //
+        // GET: /Main/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Resume()
+        {
+            return View();
+        }
+
+        public ActionResult ResumePdf()
+        {
+            return new Rotativa.ActionAsPdf("Resume")
+                    {
+                        PageOrientation = Rotativa.Options.Orientation.Portrait,
+                        PageSize = Rotativa.Options.Size.A4,
+                        PageMargins = new Rotativa.Options.Margins(0, 0, 0, 0)
+                    };
+        }
+
+        public ActionResult JBabyUsa()
+        {
+            return View("/Views/Jbabyusa/Index.cshtml");
+        }
+    }
+}
